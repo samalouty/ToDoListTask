0c80494850e24a52b5a1fa260b0ca65935d46d41
samalouty <128055267+samalouty@users.noreply.github.com>
2024-07-29 12:08:47 +0300

    added the tables

---
+++ WebApplication2/Appcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2
+{
+    public class Appcontext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; } // DbSet is like a table
+        public DbSet<Project> Projects { get; set; } 
+        public DbSet<Department> Departments { get; set; } 
+        public DbSet<CarLicense> CarLicenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
